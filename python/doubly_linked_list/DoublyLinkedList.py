71395b8f534e73362150111e9bc8063d5c1dd2fa
David Olivares <daveolivares01@gmail.com>
2024-12-23 16:05:10 -0800

    enh(doubly_linked_list): adds append method enh(doubly_linked_list): adds print_list_method

--- python/doubly_linked_list/DoublyLinkedList.py
+++ python/doubly_linked_list/DoublyLinkedList.py
@@ -6,3 +6,21 @@ class DoublyLinkedList:
         self.head = new_node
         self.tail = new_node
         self.length = 1
+
+    def print_list(self):
+        temp = self.head
+        while temp:
+            print(temp.value)
+            temp = temp.next
+
+    def append(self, value):
+        new_node = Node(value)
+        if self.head is None:
+            self.head = new_node
+            self.tail = new_node
+        else:
+            self.tail.next = new_node
+            new_node.prev = self.tail
+            self.tail = new_node
+        self.length += 1
+        return True
--- python/tests/test_doubly_linked_list.py
+++ python/tests/test_doubly_linked_list.py
@@ -13,3 +13,63 @@ def test_linked_list_initialization():
 
     # Check that length is correctly set
     assert doubly_linked_list.length == 1
+
+# Test DoublyLinkedList print_list
+def test_print_list(capsys):
+    # Test Case 1: Print list with a single element
+    dll = DoublyLinkedList(10)
+    dll.print_list()
+    captured = capsys.readouterr()
+    assert captured.out == "10\n"
+
+    # Test Case 2: Print list with multiple elements
+    dll.append(20)
+    dll.append(30)
+    dll.print_list()
+    captured = capsys.readouterr()
+    assert captured.out == "10\n20\n30\n"
+
+    # Test Case 3: Print an empty list
+    dll = DoublyLinkedList(10)
+    dll.head = None  # Simulate an empty list
+    dll.tail = None
+    dll.length = 0
+    dll.print_list()
+    captured = capsys.readouterr()
+    assert captured.out == ""
+
+# Test DoublyLinkedList apend
+def test_append(self):
+    dll = DoublyLinkedList(0)
+    # dll.pop()
+
+    # FIXME: uncomment after pop method added to create empty list.
+    #        change assertions for dll.head.value == 0 to dll.head.value == 1
+    # Test appending to an empty list
+    # dll.append(1)
+    # assert dll.head.value == 1  # Head should be the first node
+    # assert dll.tail.value == 1  # Tail should also be the first node
+    # assert dll.head.next is None  # Head next should be None
+    # assert dll.head.prev is None  # Head prev should be None
+    # assert dll.tail.next is None  # Tail next should be None
+    # assert dll.tail.prev is None  # Tail prev should be None
+    # assert dll.length == 1  # Length should be 1
+
+    # Test appending another element
+    dll.append(2)
+    assert dll.head.value == 0  # Head remains the first node
+    assert dll.tail.value == 2  # Tail should be the new node
+    assert dll.head.next == dll.tail  # Head's next should point to the new node
+    assert dll.tail.prev == dll.head  # Tail's prev should point to the old head
+    assert dll.tail.next is None  # Tail next should be None
+    assert dll.length == 2  # Length should be 2
+
+    # Test appending a third element
+    dll.append(3)
+    assert dll.head.value == 0  # Head remains the same
+    assert dll.tail.value == 3  # Tail should be the latest node
+    assert dll.head.next.value == 2  # Head's next should still be the second node
+    assert dll.tail.prev.value == 2  # Tail's prev should be the second node
+    assert dll.length == 3  # Length should be 3
+
+
